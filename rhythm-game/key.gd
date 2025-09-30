aa2ad42e63ce61fb1b9318235ff623ce92a12c69
Evan <etbay10@gmail.com>
2025-09-30 15:35:48 -0700

    made key spawn adjust to window size

--- rhythm-game/key.gd
+++ rhythm-game/key.gd
@@ -2,9 +2,13 @@ class_name Key extends Node2D
 
 var direction: Vector2 = Vector2.ZERO
 var start: Vector2
+var speed: int
+
+func _ready() -> void:
+	speed = randi_range(1,4)
 
 func _process(delta: float) -> void:
-	self.position += direction * 5
+	self.position += direction * speed
 	if abs(self.position.x - start.x) > 1500 or abs(self.position.y - start.y) > 1500:
 		self.free()
 
--- rhythm-game/key_test.gd
+++ rhythm-game/key_test.gd
@@ -2,39 +2,50 @@ extends Node2D
 
 @export var key : PackedScene
 
-const SPEED: int = 5
-
 var new_key
 var direction: Vector2 = Vector2.ZERO
-var offset: int = 100
+var offset: int = 3
 
-var above_origin: Vector2 = Vector2(576,-30)
-var below_origin: Vector2 = Vector2(576,660)
-var left_origin: Vector2 = Vector2(0,324)
-var right_origin: Vector2 = Vector2(1200,324)
+var above_origin: Vector2
+var below_origin: Vector2
+var left_origin: Vector2
+var right_origin: Vector2
 
-func _process(delta: float) -> void:
+func _ready() -> void:
+	@warning_ignore("integer_division")
+	above_origin = Vector2(get_window().size.x / 2, -30)
+	@warning_ignore("integer_division")
+	below_origin = Vector2(get_window().size.x / 2, get_window().size.y)
+	@warning_ignore("integer_division")
+	left_origin = Vector2(0, get_window().size.y / 2)
+	@warning_ignore("integer_division")
+	right_origin = Vector2(get_window().size.x, get_window().size.y / 2)
+	get_viewport()
+	
+	get_window()
+
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("key_test_down"):
 		new_key = key.instantiate()
 		if new_key is Key:
 			new_key.direction = Vector2(0,1)
-			new_key.set_key_position(above_origin + Vector2(randi_range(-offset, offset),0))
+			new_key.set_key_position(above_origin + Vector2(20 * randi_range(-offset, offset),0))
 		self.add_child(new_key)
 	if Input.is_action_just_pressed("key_test_up"):
 		new_key = key.instantiate()
 		if new_key is Key:
 			new_key.direction = Vector2(0,-1)
-			new_key.set_key_position(below_origin + Vector2(randi_range(-offset, offset),0))
+			new_key.set_key_position(below_origin + Vector2(20 * randi_range(-offset, offset),0))
 		self.add_child(new_key)
 	if Input.is_action_just_pressed("key_test_left"):
 		new_key = key.instantiate()
 		if new_key is Key:
 			new_key.direction = Vector2(-1,0)
-			new_key.set_key_position(right_origin + Vector2(0,randi_range(-offset, offset)))
+			new_key.set_key_position(right_origin + Vector2(0,20 * randi_range(-offset, offset)))
 		self.add_child(new_key)
 	if Input.is_action_just_pressed("key_test_right"):
 		new_key = key.instantiate()
 		if new_key is Key:
 			new_key.direction = Vector2(1,0)
-			new_key.set_key_position(left_origin + Vector2(0,randi_range(-offset, offset)))
+			new_key.set_key_position(left_origin + Vector2(0,20 * randi_range(-offset, offset)))
 		self.add_child(new_key)
